21539e56dd9f78e84bc48e492efff855df5143ab
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-12-17 08:47:41 +0530

    add more test cases for parseInput()

--- test/handleInputTest.js
+++ test/handleInputTest.js
@@ -3,9 +3,20 @@ const { parseInput } = require("../src/inputHandler/handleInput.js");
 
 describe("parseInput", function() {
   describe("With no options", function() {
-    it('should return an object with option as "n", count as "10", and fileNames as an array of file names', function() {
+    it('should return an object { option: "n", count: "10", fileNames: ["fileName"] } when we give only one file name', function() {
       actualInput = ["sample.txt"];
       expectedOutput = { option: "n", count: "10", fileNames: ["sample.txt"] };
+      actualOutput = parseInput(actualInput);
+
+      assert.deepEqual(actualOutput, expectedOutput);
+    });
+    it('should return an object { option: "n", count: "10", fileNames: ["fileName1","fileName2"] } when given more than one file name', function() {
+      actualInput = ["sample1.txt", "sample2.txt"];
+      expectedOutput = {
+        option: "n",
+        count: "10",
+        fileNames: ["sample1.txt", "sample2.txt"]
+      };
       actualOutput = parseInput(actualInput);
 
       assert.deepEqual(actualOutput, expectedOutput);
