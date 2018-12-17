c4e5a95d79d96d369cc4e9f74de6f19052fdfc57
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-12-17 15:52:56 +0530

    modify parseInput to handle options

--- src/inputHandler/handleInput.js
+++ src/inputHandler/handleInput.js
@@ -1,5 +1,57 @@
+const {} = require("../util/utils.js");
+
+const hasOption = function(userInputs) {
+  return userInputs[0].slice(0, 1) == "-";
+};
+
+const getOption = function(userOption) {
+  let option = userOption.slice(1, 2);
+
+  if (!isNaN(option)) {
+    option = "n";
+  }
+  return option;
+};
+
+const isLengthTwo = function(string) {
+  return string.length == 2;
+};
+
+const hasOnlyOption = function(option) {
+  return isLengthTwo(option) && isNaN(option.slice(1, 2));
+};
+
+const getLineCount = function(userInputs) {
+  let option = userInputs[0];
+
+  if (hasOnlyOption(option)) {
+    return userInputs[1];
+  }
+
+  if (isLengthTwo(option)) {
+    return option.slice(1);
+  }
+
+  return option.slice(2);
+};
+
+const getFileNames = function(userInputs) {
+  if (hasOnlyOption(userInputs[0])) {
+    return userInputs.slice(2);
+  }
+  return userInputs.slice(1);
+};
+
 const parseInput = function(userInputs) {
-  return { option: "n", count: "10", fileNames: userInputs };
+  let parsedInput = { option: "n", count: "10", fileNames: userInputs };
+
+  if (hasOption(userInputs)) {
+    parsedInput.option = getOption(userInputs[0]);
+    parsedInput.count = getLineCount(userInputs);
+    parsedInput.fileNames = getFileNames(userInputs);
+  }
+
+  return parsedInput;
 };
 
 module.exports = { parseInput };
--- test/handleInputTest.js
+++ test/handleInputTest.js
@@ -4,20 +4,38 @@ const { parseInput } = require("../src/inputHandler/handleInput.js");
 describe("parseInput", function() {
   describe("With no options", function() {
     it('should return an object { option: "n", count: "10", fileNames: ["fileName"] } when we give only one file name', function() {
-      actualInput = ["sample.txt"];
-      expectedOutput = { option: "n", count: "10", fileNames: ["sample.txt"] };
-      actualOutput = parseInput(actualInput);
+      let userInput = ["sample.txt"];
+      let expectedOutput = {
+        option: "n",
+        count: "10",
+        fileNames: ["sample.txt"]
+      };
+      let actualOutput = parseInput(userInput);
 
       assert.deepEqual(actualOutput, expectedOutput);
     });
     it('should return an object { option: "n", count: "10", fileNames: ["fileName1","fileName2"] } when given more than one file name', function() {
-      actualInput = ["sample1.txt", "sample2.txt"];
-      expectedOutput = {
+      let userInput = ["sample1.txt", "sample2.txt"];
+      let expectedOutput = {
         option: "n",
         count: "10",
         fileNames: ["sample1.txt", "sample2.txt"]
       };
-      actualOutput = parseInput(actualInput);
+      let actualOutput = parseInput(userInput);
+
+      assert.deepEqual(actualOutput, expectedOutput);
+    });
+  });
+
+  describe("With options", function() {
+    it('should return an object with option:"n",count:"1" and filenames when given option as -n1 ', function() {
+      let userInput = ["-n1", "sample.js"];
+      let expectedOutput = {
+        option: "n",
+        count: "1",
+        fileNames: ["sample.js"]
+      };
+      let actualOutput = parseInput(userInput);
 
       assert.deepEqual(actualOutput, expectedOutput);
     });
