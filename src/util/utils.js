e69a67c07ec1d57b3129054481a7c3420db7eecf
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-12-18 08:57:59 +0530

    modify getTopLines to return string

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,10 +1,11 @@
 const getTopLines = function(contents, numberOfLines) {
   let data = contents.split("\n");
-  return data.slice(0, numberOfLines);
+  return data.slice(0, numberOfLines).join("\n");
 };
 
 const getFirstCharacters = function(contents, numberOfCharacters) {
   let data = contents.split("");
   return data.slice(0, numberOfCharacters);
 };
+
 module.exports = { getTopLines, getFirstCharacters };
--- test/utilTest.js
+++ test/utilTest.js
@@ -4,24 +4,24 @@ const { getTopLines, getFirstCharacters } = require("../src/util/utils.js");
 describe("getTopLines", function() {
   it("should return an empty array when the number of lines is 0", function() {
     let contents = "a";
-    let expectedOutput = [];
+    let expectedOutput = "";
     let actualOutput = getTopLines(contents, 0);
 
     assert.deepEqual(actualOutput, expectedOutput);
   });
 
   it("should return an array of empty string when the content is an empty string", function() {
-    assert.deepEqual(getTopLines("", 2), [""]);
+    assert.deepEqual(getTopLines("", 2), "");
   });
 
   it("should return an array with 1 elements when the number of lines is 1", function() {
     let contents = "1\n2\n3";
-    assert.deepEqual(getTopLines(contents, 1), ["1"]);
+    assert.deepEqual(getTopLines(contents, 1), "1");
   });
 
   it("should return an array with 2 elements when the number of lines is 2", function() {
     let contents = "1\n2\n3\n4";
-    assert.deepEqual(getTopLines(contents, 2), ["1", "2"]);
+    assert.deepEqual(getTopLines(contents, 2), "1\n2");
   });
 });
 
