a4bb429f9a9228c9fb2134d73e7a5dfa6ce750a2
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-12-20 08:18:49 +0530

    add testFile for head.js

--- head.js
+++ head.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const { parseInput } = require("./src/inputHandler/handleInput.js");
-const { head } = require("./src/library/headLibrary.js");
+const { filter } = require("./src/library/headLibrary.js");
 
 const main = function(args) {
   let userInputs = parseInput(args.slice(2), "head");
-  let result = head(userInputs, fs);
+  let result = filter(userInputs, fs);
 
   result.map(x => console.log(x));
 };
