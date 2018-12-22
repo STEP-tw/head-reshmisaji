9add94c4821b9d497214d0a5535aacde141236c6
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-12-22 19:33:01 +0530

    modify main() of head and tail to remove the unwanted spaces at the end

--- head.js
+++ head.js
@@ -6,6 +6,6 @@ const main = function(args) {
   let userInputs = parseInput(args.slice(2), "head");
   let result = filter(userInputs, fs);
 
-  result.map(x => console.log(x));
+  result.map(x => console.log(x.trim()));
 };
 main(process.argv);
